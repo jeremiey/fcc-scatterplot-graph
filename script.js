61694d46ef6624f21ed7a71105d0fb6e8ec3c4ca
Jeremiah Aworetan <aworetanjeremiah@gmail.com>
2022-03-15 23:23:28 +0100

    made some rearrangements and added a tooltip

--- script.js
+++ script.js
@@ -5,85 +5,103 @@ let values = []
 let xScale
 let yScale
 
+let xAxis
+let yAxis
+
 let width = 800
 let height = 600
 let padding = 40
 
 let svg = d3.select('svg')
+let tooltip = d3.select('#tooltip')
+
+let generateScales = () => {
+    
+  xScale = d3.scaleLinear()
+             .domain([d3.min(values, (item) => {
+               return item['Year']
+             }) - 1 , d3.max(values, (item) => {
+               return item['Year']
+             }) + 1])
+             .range([padding, width-padding])
+
+  yScale = d3.scaleTime()
+             .domain([d3.min(values, (item) => {
+               return new Date(item['Seconds'] * 1000)
+             }), d3.max(values, (item) => {
+              return new Date(item['Seconds'] * 1000)
+             })])
+             .range([padding, height-padding])
+
+}
 
 let drawCanvas = () => {
   svg.attr('width', width)
-     .attr('height', height)
-}
-
-let generateScales = () => {
-
-  xScale = d3.scaleLinear()
-             .domain([d3.min(values, (item) => {
-               return item['Year'] - 1
-             }), d3.max(values, (item) => {
-               return item['Year'] + 1
-             })])
-             .range([padding, width - padding])
-
-  yScale = d3.scaleLinear()
-             .domain([d3.min(values, (item) => {
-               return new Date(item['Seconds'] * 1000)
-             }), d3.max(values, (item) => {
-               return new Date(item['Seconds'] * 1000)
-             })])
-             .range([height - padding, padding])
-
+  svg.attr('height', height)
 }
 
 let drawPoints = () => {
 
   svg.selectAll('circle')
-     .data(values)
-     .enter()
-     .append('circle')
-     .attr('class', 'dot')
-     .attr('r', '5')
-     .attr('data-xvalue', (item) => {
-       return item['Year']
-     })
-     .attr('data-yvalue', (item) => {
-       return new Date(item['Seconds'] * 1000) // converted the time in seconds to milliseconds
-     })
-     .attr('cx', (item) => {
-       return xScale(item['Year'])
-     })
-     .attr('cy', (item) => {
-       return yScale(new Date(item['Seconds'] * 1000))
-     })
-     .attr('fill', (item) => {
-       if(item['Doping'] != '') {
-         return 'orange'
-       } else {
-         return 'lightgreen'
-       }
-     })
-
+    .data(values)
+    .enter()
+    .append('circle')
+    .attr('class', 'dot')
+    .attr('r', '5')
+    .attr('data-xvalue', (item) => {
+    return item['Year']
+    })
+    .attr('data-yvalue', (item) => {
+      return new Date(item['Seconds'] * 1000)
+    })
+    .attr('cx', (item) => {
+      return xScale(item['Year'])
+    })         
+    .attr('cy', (item) => {
+      return yScale(new Date(item['Seconds'] * 1000))
+    })
+    .attr('fill', (item) => {
+        if(item['URL'] === "") {
+          return 'lightgreen'
+        } else {
+          return 'orange'
+        }
+    })
+    .on('mouseover', (item) => {
+      tooltip.transition()
+             .style('visibility', 'visible')
+      
+      if(item['Doping'] != "") {
+        tooltip.text(item['Year'] + ' - ' + item['Name'] + ' - ' + item['Time'] + ' - ' + item['Doping'])
+      } else {
+        tooltip.text(item['Year'] + ' - ' + item['Name'] + ' - ' + item['Time'] + ' - ' + 'No Allegations')
+      }
+      
+      tooltip.attr('data-year', item['Year'])
+    })
+    .on('mouseout', (item) => {
+        tooltip.transition()
+               .style('visibility', 'hidden')
+    })
 }
 
 let generateAxes = () => {
 
-  let xAxis = d3.axisBottom(xScale)
-                .tickFormat(d3.format('d')) // removes the comma in x-axis values
+    xAxis = d3.axisBottom(xScale)
+              .tickFormat(d3.format('d'))
+                
+    yAxis = d3.axisLeft(yScale)
+              .tickFormat(d3.timeFormat('%M:%S'))
 
-  let yAxis = d3.axisLeft(yScale)
-                .tickFormat(d3.timeFormat('%M:%S')) // displays y-axis values in MM:SS format
+    svg.append('g')
+       .call(xAxis)
+       .attr('id', 'x-axis')
+       .attr('transform', 'translate(0, ' + (height-padding) +')')
 
-  svg.append('g')
-     .call(xAxis)
-     .attr('id', 'x-axis')
-     .attr('transform', `translate(0, ${height - padding})`)
-
-  svg.append('g')
-     .call(yAxis)
-     .attr('id', 'y-axis')
-     .attr('transform', `translate(${padding}, 0)`)
-
+    svg.append('g')
+       .call(yAxis)
+       .attr('id', 'y-axis')
+       .attr('transform', `translate(${padding}, 0)`)
 }
 
 fetch(url)
@@ -94,4 +112,4 @@ fetch(url)
     generateScales()
     drawPoints()
     generateAxes()
-  })
+})
